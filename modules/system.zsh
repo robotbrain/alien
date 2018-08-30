db675aeb30af11afd64489e0a12f15cb4e7917c7
Sky <robotbrainify@gmail.com>
2018-08-30 17:27:35 -0400

    Update system.zsh

--- modules/system.zsh
+++ modules/system.zsh
@@ -19,36 +19,3 @@ alien_ssh_client(){
     echo -n $SSH_CLIENT | awk {'print $1 " "'};
   fi
 }
-
-alien_battery_stat(){
-  __os=`uname`;
-  if [[ $__os = "Linux" ]]; then
-    if which upower > /dev/null 2>&1 ; then
-      __bat_power=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | awk '{print $2}'`;
-      __bat_power_ind="";
-      if [[ $__bat_power = "charging" ]]; then __bat_power_ind="+";
-      elif [[ $__bat_power = "discharging" ]]; then __bat_power_ind="-";
-      elif [[ $__bat_power = "fully-charged" ]]; then __bat_power_ind="•";
-      fi
-      __bat_per=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | awk '{print $2}' | sed "s|%||g"`;
-      if [[ -n $__bat_per ]]; then
-        echo -n " | ${__bat_power_ind}${__bat_per}";
-      fi
-    fi
-  fi
-  if [[ $__os = "Darwin" ]]; then
-    __bat_power=`pmset -g batt | tail -1 | awk '{print $4}' | tr -d "%;"`;
-    __bat_power_ind="";
-    if [[ $__bat_power = "charging" ]]; then __bat_power_ind="+";
-    elif [[ $__bat_power = "discharging" ]]; then __bat_power_ind="-";
-    elif [[ $__bat_power = "finishing" ]]; then __bat_power_ind="+";
-    elif [[ $__bat_power = "charged" ]]; then __bat_power_ind="•";
-    fi
-       __bat_per=`pmset -g batt | tail -1 | awk '{print $3}' | tr -d "%;"`
-    if [[ -n $__bat_per ]]; then
-      echo -n " | ${__bat_power_ind}${__bat_per}";
-    fi
-  fi
-
-  unset __bat_power __bat_power_ind __bat_per
-}
